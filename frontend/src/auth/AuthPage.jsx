1862ab58440ed2d5818c144c0e1d1bd5b3349460
AbolfazlAdhami <aadhami880@gmail.com>
2025-06-25 04:11:02 +0330

    feat(auth): clerk modal OAuth

--- frontend/src/auth/AuthPage.jsx
+++ frontend/src/auth/AuthPage.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { SignIn, SignUp, SignedIn, SignedOut } from "@clerk/clerk-react";
 
 function AuthPage() {
   return (
-    <div>AuthPage</div>
-  )
+    <main className="min-w-[400px] w-screen h-screen my-8 mx-auto bg-slate-50 rounded-lg shadow flex flex-col items-center justify-center">
+      <SignedOut>
+        <SignIn routing="path" path="/sign-in" />
+        <SignUp routing="path" path="/sign-up" />
+      </SignedOut>
+      <SignedIn>
+        <div className="text-center text-text mt-4">
+          <p>You are already signed in.</p>
+        </div>
+      </SignedIn>
+    </main>
+  );
 }
 
-export default AuthPage
+export default AuthPage;
--- frontend/src/auth/ClerkProviderWithRoutes.jsx
+++ frontend/src/auth/ClerkProviderWithRoutes.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const PUBLISHABLE_KEY = import.meta.env.PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
 
 function ClerkProviderWithRoutes({ children }) {
+  console.log(PUBLISHABLE_KEY, "PUBLISHABLE_KEY");
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <Router>{children}</Router>
