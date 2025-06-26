c324bb6fcdcbbf00a676408824e5c60f3a63f6dd
AbolfazlAdhami <aadhami880@gmail.com>
2025-06-27 02:33:59 +0330

    feat: challenge page functionallity

---
+++ frontend/src/utils/useFetch.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+import { useApi } from "../utils/api.js";
+
+export function useFetch() {
+  const [quota, setQuota] = useState(null);
+  const { makeRequest } = useApi();
+  const fetchQuota = async () => {
+    try {
+      const data = await makeRequest("quota");
+      setQuota(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuota();
+  }, []);
+
+  return { quota };
+}
