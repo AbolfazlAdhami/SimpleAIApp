4ce2152e74135be73b4ad80ad76c501d79e2f0a5
AbolfazlAdhami <aadhami880@gmail.com>
2025-06-28 16:46:09 +0330

    chor(config): .gitignore add .env files

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,2 +1,10 @@
 from fastapi import FastApi, Request, Response
-from fastapi import 
+from fastapi.middleware.cors import CORSMiddleware
+from clerk_backend_api import Clerk
+import os
+
+
+clerk_sdk = Clerk(os.getenv("CLERK_SECRET_KEY"))
+
+app = FastApi()
+app.add
